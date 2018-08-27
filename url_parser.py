44a2dc2c36c2f5608f7fd60455a0897c70b78ae6
YvonneLi <wenli@wens-MacBook.local>
2018-08-27 17:01:07 -0400

    work_version. update(url.decode(‘utf-8’))

--- url_parser.py
+++ url_parser.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 import urllib2
 import urlparse
 from urlparse import urljoin
@@ -15,19 +16,42 @@ class MyHTMLParser(HTMLParser):
         	if attribute == 'href':
         		url = urljoin(base_url,value)
         		links.add(url)
-    
-def extract_url(cur_url):
-    
-	scr = urllib2.urlopen(cur_url)
-	html = scr.read()
 
-	parser = MyHTMLParser()
-	parser.feed(html)
+class Web_crawl(): 
 
-	new_res = []
-	
-	for l in links:
-		new_res.append(l)
-	return new_res
+	def extract_url(self,cur_url):
 
-print(extract_url('http://en.wikipedia.org/')) 	
+		try:
+			f = urllib2.urlopen(cur_url)
+			html = f.read()
+		except urllib2.HTTPError as e:
+			return []
+
+		html = html.decode('utf-8')
+		parser = MyHTMLParser()
+		parser.feed(html)
+
+		new_res = []
+		for l in links:
+			new_res.append(l)
+		return new_res
+
+
+	def crawlbfs(self,url):
+		queue  = []
+		result = set()
+		queue.append(url)
+
+		while queue:
+			cur = queue.pop(0)
+			print(cur)
+			cur_children = self.extract_url(cur)
+
+			if len(cur_children) != 0:
+				for nxt in cur_children:
+					if nxt not in result:
+							queue.append(nxt)
+							result.add(nxt)
+		return result
+
+print(Web_crawl().crawlbfs('https://commons.wikimedia.org/wiki/Main_Page')) 	
